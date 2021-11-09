76e294f8d9ff819353bef9251d1e1acf4ada0f95
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-11-09 14:15:30 +0900

    Create CameraApp

--- CameraApp/ViewController.swift
+++ CameraApp/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        imageView.backgroundColor = .secondarySystemBackground
+        
+        button.backgroundColor = .systemBlue
+        button.setTitle("Take Picture", for: .normal)
+        button.setTitleColor(.white, for: .normal)
     }
 
+    @IBAction private func didTapButton() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true//以下のimageでInfoKey.editedImageとする場合、trueにする必要あり
+        picker.delegate = self
+        present(picker, animated: true)
+    }
 
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+//        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+//            return
+//        }
+
+        // editImageにすると、撮影後に画像のトリミングが可能
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        
+        imageView.image = image
+    }
+    
+    
+}
